84d20ad569444093a7a0b319533570ce1864737f
pratapshingane18 <sushrutrao465@gmail.com>
2024-05-16 12:04:07 +0530

    some updates

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, pratap!');
+});
+
+app.get('/home', (req, res) => {
+    res.send('This is home!');
 });
 
 app.listen(port, () => {
